92992573db098481c42647f8a42b434799457b2a
shatVM <pzs_shvm@ztu.edu.ua>
2023-06-06 15:41:13 +0300

    Update app.js

--- atestat/app.js
+++ atestat/app.js
@@ -1,4 +1,23 @@
-console.clear()
+//[START Видалення всіх оцінок http://atestat.pp.ua/]
+var delBtn = document.querySelectorAll('.ozuserlist')
+
+delBtn.forEach(element => {
+    element.click()
+    var delBtn = document.querySelectorAll('.deltr')
+
+delBtn.forEach(element => {
+    element.click()
+});
+});
+//[END Видалення всіх оцінок http://atestat.pp.ua/]
+
+var toz = document.querySelectorAll('.toz')
+
+var allElements = document.getElementsByClassName('toz');
+console.log(allElements)
+
+
+
 // Отримуємо всі елементи <td> з класом "prname" (назви предметів)
 var prnameElements = document.querySelectorAll('td.prname');
 
@@ -10,70 +29,38 @@ prnameElements.forEach(function(element) {
   // Знаходимо відповідний елемент <td> з класом "toz" для даного предмета
   var tozElement = element.nextElementSibling;
 
-if (tozElement) {
-    
-    simulateEnterKeyPress(tozElement);
-  }
-
-    function simulateEnterKeyPress(element) {
-  // Клік на елементі
-  element.click();
-    element.innerText = grade.toString();   
-    var event = new KeyboardEvent('keydown', {
-        key: 'Enter',
-        keyCode: 13
-      });
-
-      element.dispatchEvent(event);
-
-  setTimeout(function() {
-    // Затримка 1 секунда перед внесенням даних
-  }, 1000); // Затримка 1 секунда
-}
-
+  // Встановлюємо отриману оцінку у внутрішній текстовий елемент <td>
+  tozElement.innerText = grade.toString();
 });
-
-
 
 // Функція для отримання оцінки для кожного предмета
 function getGradeForSubject(subject) {
+    // Отримання даних з буфера обміну
+    //var clipboardData = navigator.clipboard.getData('Text');
+    var clipboardData = marks;
 
-  var grades =     {
-        "Прізвище": "Бондар В.В.",
-        "Номер свідоцтва": 53705532,
-        "Українська мова": 10,
-        "Українська література": 10,
-        "Англійська мова": 8,
-        "Зарубіжна література": 10,
-        "Історія України": 9,
-        "Всесвітня історія": 10,
-        "Громадянська освіта": "",
-        "Математика": 10,
-        "Природничі науки": 10,
-        "Захист України": "",
-        "Фізична культура": 11,
-        "Практичні заняття з інформатики": 11,
-        "Хімія: лабораторні та практичні розв’язування задач": 10,
-        "Біологія: розв’язування задач": 12,
-        "Фізика:лабораторні та практичні розв’язування задач": 10,
-        "Географія": 11,
-        "Інформатика": "",
-        "Технології": 11,
-        "Країнознавство Великобританії та США": "",
-        "Англійська ділова мова": ""
-    };
-
-    
-  if (subject in grades) {
-    var grade = grades[subject];
-    if (grade !== "") {
-      if (!isNaN(grade)) {
+  
+    // Розбиття даних на рядки
+    var rows = clipboardData.split('\n');
+  
+    // Пошук рядка з предметом
+    var subjectRow = rows.find(function(row) {
+      return row.includes(subject);
+    });
+  
+    if (subjectRow) {
+      // Розбиття рядка на колонки
+      var columns = subjectRow.split('\t');
+  
+      // Отримання оцінки для предмета
+      var grade = columns[1];
+  
+      if (grade && !isNaN(grade)) {
         return parseInt(grade);
-      } else {
-        return 0; // Встановлюємо оцінку 0, якщо значення не є числом
       }
     }
+  
+    return 0; // Встановлюємо оцінку 0, якщо не вдалося отримати оцінку з буфера обміну
   }
-
-  return ""; // Повертаємо порожнє значення, якщо оцінка для предмета не знайдена
-}
+  
+  marks = 'Українська мова	Українська література	Англійська мова	Зарубіжна література	Історія України	Всесвітня історія	Громадянська освіта	Математика	Природничі науки	Захист України	Фізична культура	Практичні заняття з інформатики	Хімія: лабораторні та практичні розв’язування задач	Біологія: розв’язування задач	Фізика: лабораторні та практичні розв’язування задач	Географія 	Інформатика 	Технології	Країнознавство Великобританії та США	Англійська ділова мова   10	10	8	10	9	10		10	10		11	11	10	12	10	11		11		'
