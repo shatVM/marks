c7e7a56f1d4193f49a5f990834ca23a49473d5d0
shatVM <90845743+shatVM@users.noreply.github.com>
2024-01-15 17:19:51 +0200

    Update del_lessons_nz.js

--- lessons/del_lessons_nz.js
+++ lessons/del_lessons_nz.js
@@ -8,15 +8,12 @@ lessons[0].click();
 setTimeout(function () {
   document.querySelector(".delete-lesson").click();
   setTimeout(function () {
-    // Найти все элементы <button> на странице
-
+   
     const buttons = document.querySelectorAll("button");
-
-    buttons[5].click();
+    buttons.forEach(function(button) {
+      if (button.innerText === " Так") {    
+        button.click()    
+       }
+    });   
   }, 500);
 }, 500);
-
-
-
-
-
